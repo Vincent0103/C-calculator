c3af42ae8dd09b9ad1dd9f07ff4397c5cb8ca643
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-04-06 20:09:01 +0200

    refactor numbers array

--- main.c
+++ main.c
@@ -15,17 +15,17 @@ float calculate(int a, char op, int b) {
 }
 
 int main(int argc, char *argv[]) {
-  int numbers[2];
+  int a, b;
   char op;
   float result;
 
   printf("Enter in two numbers delimited by a space: ");
-  scanf("%d %d", &numbers[0], &numbers[1]);
+  scanf("%d %d", &a, &b);
 
   printf("what operation would you like to perform? ");
   scanf(" %c", &op);
 
-  result = calculate(numbers[0], op, numbers[1]);
-  printf("%d %c %d = %.1f\n", numbers[0], op, numbers[1], result);
+  result = calculate(a, op, b);
+  printf("%d %c %d = %.1f\n", a, op, b, result);
   return 0;
 }
